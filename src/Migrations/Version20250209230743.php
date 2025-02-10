26c2576b5506a52e17382c30222294ddae85fde5
Christoph Kappestein <christoph.kappestein@gmail.com>
2025-02-10 21:42:16 +0100

    add demo data

--- src/Migrations/Version20250209230743.php
+++ src/Migrations/Version20250209230743.php
@@ -4,6 +4,7 @@ namespace App\Migrations;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
+use Ramsey\Uuid\Uuid;
 
 /**
  *
@@ -97,5 +98,53 @@ class Version20250209230743 extends AbstractMigration
         $schema->dropTable('app_organization');
         $schema->dropTable('app_event');
         $schema->dropTable('app_event_attendees');
+    }
+
+    /**
+     * Manually added to insert some demo data
+     */
+    public function postUp(Schema $schema): void
+    {
+        $rows = [
+            ['user_id' => 2, 'status' => 1, 'display_id' => Uuid::uuid4()->toString(), 'name' => 'Google', 'legal_name' => 'GOOGLE LLC', 'lei_code' => '7ZW8QJWVPR4P1J1KQY45', 'update_date' => date('Y-m-d H:i:s'), 'insert_date' => date('Y-m-d H:i:s')],
+            ['user_id' => 2, 'status' => 1, 'display_id' => Uuid::uuid4()->toString(), 'name' => 'Microsoft', 'legal_name' => 'MICROSOFT CORPORATION', 'lei_code' => 'INR2EJN1ERAN0W5ZP974', 'update_date' => date('Y-m-d H:i:s'), 'insert_date' => date('Y-m-d H:i:s')],
+            ['user_id' => 2, 'status' => 1, 'display_id' => Uuid::uuid4()->toString(), 'name' => 'Apple', 'legal_name' => 'Apple Inc.', 'lei_code' => 'HWUPKR0MPOU8FGXBT394', 'update_date' => date('Y-m-d H:i:s'), 'insert_date' => date('Y-m-d H:i:s')],
+        ];
+        foreach ($rows as $row) {
+            $this->connection->insert('app_organization', $row);
+        }
+
+        $rows = [
+            ['user_id' => 2, 'status' => 1, 'display_id' => Uuid::uuid4()->toString(), 'name' => 'Eric Schmidt', 'family_name' => 'Schmidt', 'given_name' => 'Eric', 'email' => '', 'birth_date' => '1955-04-27', 'member_of_id' => 1, 'update_date' => date('Y-m-d H:i:s'), 'insert_date' => date('Y-m-d H:i:s')],
+            ['user_id' => 2, 'status' => 1, 'display_id' => Uuid::uuid4()->toString(), 'name' => 'Sergey Brin', 'family_name' => 'Brin', 'given_name' => 'Sergey', 'email' => '', 'birth_date' => '1973-08-21', 'member_of_id' => 1, 'update_date' => date('Y-m-d H:i:s'), 'insert_date' => date('Y-m-d H:i:s')],
+            ['user_id' => 2, 'status' => 1, 'display_id' => Uuid::uuid4()->toString(), 'name' => 'Larry Page', 'family_name' => 'Page', 'given_name' => 'Larry', 'email' => '', 'birth_date' => '1973-03-26', 'member_of_id' => 1, 'update_date' => date('Y-m-d H:i:s'), 'insert_date' => date('Y-m-d H:i:s')],
+            ['user_id' => 2, 'status' => 1, 'display_id' => Uuid::uuid4()->toString(), 'name' => 'Bill Gates', 'family_name' => 'Gates', 'given_name' => 'Bill', 'email' => '', 'birth_date' => '1955-10-28', 'member_of_id' => 2, 'update_date' => date('Y-m-d H:i:s'), 'insert_date' => date('Y-m-d H:i:s')],
+            ['user_id' => 2, 'status' => 1, 'display_id' => Uuid::uuid4()->toString(), 'name' => 'Paul Allen', 'family_name' => 'Allen', 'given_name' => 'Paul', 'email' => '', 'birth_date' => '1953-01-21', 'member_of_id' => 2, 'update_date' => date('Y-m-d H:i:s'), 'insert_date' => date('Y-m-d H:i:s')],
+            ['user_id' => 2, 'status' => 1, 'display_id' => Uuid::uuid4()->toString(), 'name' => 'Steve Wozniak', 'family_name' => 'Wozniak', 'given_name' => 'Steve', 'email' => '', 'birth_date' => '1950-08-11', 'member_of_id' => 3, 'update_date' => date('Y-m-d H:i:s'), 'insert_date' => date('Y-m-d H:i:s')],
+            ['user_id' => 2, 'status' => 1, 'display_id' => Uuid::uuid4()->toString(), 'name' => 'Steve Jobs', 'family_name' => 'Jobs', 'given_name' => 'Steve', 'email' => '', 'birth_date' => '1955-02-24', 'member_of_id' => 3, 'update_date' => date('Y-m-d H:i:s'), 'insert_date' => date('Y-m-d H:i:s')],
+        ];
+        foreach ($rows as $row) {
+            $this->connection->insert('app_person', $row);
+        }
+
+        $rows = [
+            ['user_id' => 2, 'status' => 1, 'display_id' => Uuid::uuid4()->toString(), 'name' => 'Google I/O', 'funder_id' => 1, 'start_date' => date('Y-m-d H:i:s', strtotime('today')), 'end_date' => date('Y-m-d H:i:s', strtotime('tomorrow')), 'update_date' => date('Y-m-d H:i:s'), 'insert_date' => date('Y-m-d H:i:s')],
+        ];
+        foreach ($rows as $row) {
+            $this->connection->insert('app_event', $row);
+        }
+
+        $rows = [
+            ['event_id' => 1, 'person_id' => 1],
+            ['event_id' => 1, 'person_id' => 2],
+            ['event_id' => 1, 'person_id' => 3],
+            ['event_id' => 1, 'person_id' => 4],
+            ['event_id' => 1, 'person_id' => 5],
+            ['event_id' => 1, 'person_id' => 6],
+            ['event_id' => 1, 'person_id' => 7],
+        ];
+        foreach ($rows as $row) {
+            $this->connection->insert('app_event_attendees', $row);
+        }
     }
 }
