054d68342d3e22369d6b05ce9ef2d27776a24134
Christoph Kappestein <christoph.kappestein@gmail.com>
2025-05-01 08:18:40 +0200

    update gen

--- src/Table/Generated/EventTable.php
+++ src/Table/Generated/EventTable.php
@@ -2,6 +2,472 @@
 
 namespace App\Table\Generated;
 
-class EventTable
+/**
+ * @extends \PSX\Sql\TableAbstract<\App\Table\Generated\EventRow>
+ */
+class EventTable extends \PSX\Sql\TableAbstract
 {
+    public const NAME = 'app_event';
+    public const COLUMN_ID = 'id';
+    public const COLUMN_USER_ID = 'user_id';
+    public const COLUMN_STATUS = 'status';
+    public const COLUMN_DISPLAY_ID = 'display_id';
+    public const COLUMN_NAME = 'name';
+    public const COLUMN_FUNDER_ID = 'funder_id';
+    public const COLUMN_START_DATE = 'start_date';
+    public const COLUMN_END_DATE = 'end_date';
+    public const COLUMN_UPDATE_DATE = 'update_date';
+    public const COLUMN_INSERT_DATE = 'insert_date';
+    public function getName(): string
+    {
+        return self::NAME;
+    }
+    public function getColumns(): array
+    {
+        return [self::COLUMN_ID => 0x3020000a, self::COLUMN_USER_ID => 0x20000a, self::COLUMN_STATUS => 0x20000a, self::COLUMN_DISPLAY_ID => 0xa00024, self::COLUMN_NAME => 0x40a000ff, self::COLUMN_FUNDER_ID => 0x20000a, self::COLUMN_START_DATE => 0x800000, self::COLUMN_END_DATE => 0x800000, self::COLUMN_UPDATE_DATE => 0x800000, self::COLUMN_INSERT_DATE => 0x800000];
+    }
+    /**
+     * @return array<\App\Table\Generated\EventRow>
+     * @throws \PSX\Sql\Exception\QueryException
+     */
+    public function findAll(?\PSX\Sql\Condition $condition = null, ?int $startIndex = null, ?int $count = null, ?\App\Table\Generated\EventColumn $sortBy = null, ?\PSX\Sql\OrderBy $sortOrder = null): array
+    {
+        return $this->doFindAll($condition, $startIndex, $count, $sortBy, $sortOrder);
+    }
+    /**
+     * @return array<\App\Table\Generated\EventRow>
+     * @throws \PSX\Sql\Exception\QueryException
+     */
+    public function findBy(\PSX\Sql\Condition $condition, ?int $startIndex = null, ?int $count = null, ?\App\Table\Generated\EventColumn $sortBy = null, ?\PSX\Sql\OrderBy $sortOrder = null): array
+    {
+        return $this->doFindBy($condition, $startIndex, $count, $sortBy, $sortOrder);
+    }
+    /**
+     * @throws \PSX\Sql\Exception\QueryException
+     */
+    public function findOneBy(\PSX\Sql\Condition $condition): ?\App\Table\Generated\EventRow
+    {
+        return $this->doFindOneBy($condition);
+    }
+    /**
+     * @throws \PSX\Sql\Exception\QueryException
+     */
+    public function find(int $id): ?\App\Table\Generated\EventRow
+    {
+        $condition = \PSX\Sql\Condition::withAnd();
+        $condition->equals('id', $id);
+        return $this->doFindOneBy($condition);
+    }
+    /**
+     * @return array<\App\Table\Generated\EventRow>
+     * @throws \PSX\Sql\Exception\QueryException
+     */
+    public function findById(int $value, ?int $startIndex = null, ?int $count = null, ?\App\Table\Generated\EventColumn $sortBy = null, ?\PSX\Sql\OrderBy $sortOrder = null): array
+    {
+        $condition = \PSX\Sql\Condition::withAnd();
+        $condition->equals('id', $value);
+        return $this->doFindBy($condition, $startIndex, $count, $sortBy, $sortOrder);
+    }
+    /**
+     * @throws \PSX\Sql\Exception\QueryException
+     */
+    public function findOneById(int $value): ?\App\Table\Generated\EventRow
+    {
+        $condition = \PSX\Sql\Condition::withAnd();
+        $condition->equals('id', $value);
+        return $this->doFindOneBy($condition);
+    }
+    /**
+     * @throws \PSX\Sql\Exception\ManipulationException
+     */
+    public function updateById(int $value, \App\Table\Generated\EventRow $record): int
+    {
+        $condition = \PSX\Sql\Condition::withAnd();
+        $condition->equals('id', $value);
+        return $this->doUpdateBy($condition, $record->toRecord());
+    }
+    /**
+     * @throws \PSX\Sql\Exception\ManipulationException
+     */
+    public function deleteById(int $value): int
+    {
+        $condition = \PSX\Sql\Condition::withAnd();
+        $condition->equals('id', $value);
+        return $this->doDeleteBy($condition);
+    }
+    /**
+     * @return array<\App\Table\Generated\EventRow>
+     * @throws \PSX\Sql\Exception\QueryException
+     */
+    public function findByUserId(int $value, ?int $startIndex = null, ?int $count = null, ?\App\Table\Generated\EventColumn $sortBy = null, ?\PSX\Sql\OrderBy $sortOrder = null): array
+    {
+        $condition = \PSX\Sql\Condition::withAnd();
+        $condition->equals('user_id', $value);
+        return $this->doFindBy($condition, $startIndex, $count, $sortBy, $sortOrder);
+    }
+    /**
+     * @throws \PSX\Sql\Exception\QueryException
+     */
+    public function findOneByUserId(int $value): ?\App\Table\Generated\EventRow
+    {
+        $condition = \PSX\Sql\Condition::withAnd();
+        $condition->equals('user_id', $value);
+        return $this->doFindOneBy($condition);
+    }
+    /**
+     * @throws \PSX\Sql\Exception\ManipulationException
+     */
+    public function updateByUserId(int $value, \App\Table\Generated\EventRow $record): int
+    {
+        $condition = \PSX\Sql\Condition::withAnd();
+        $condition->equals('user_id', $value);
+        return $this->doUpdateBy($condition, $record->toRecord());
+    }
+    /**
+     * @throws \PSX\Sql\Exception\ManipulationException
+     */
+    public function deleteByUserId(int $value): int
+    {
+        $condition = \PSX\Sql\Condition::withAnd();
+        $condition->equals('user_id', $value);
+        return $this->doDeleteBy($condition);
+    }
+    /**
+     * @return array<\App\Table\Generated\EventRow>
+     * @throws \PSX\Sql\Exception\QueryException
+     */
+    public function findByStatus(int $value, ?int $startIndex = null, ?int $count = null, ?\App\Table\Generated\EventColumn $sortBy = null, ?\PSX\Sql\OrderBy $sortOrder = null): array
+    {
+        $condition = \PSX\Sql\Condition::withAnd();
+        $condition->equals('status', $value);
+        return $this->doFindBy($condition, $startIndex, $count, $sortBy, $sortOrder);
+    }
+    /**
+     * @throws \PSX\Sql\Exception\QueryException
+     */
+    public function findOneByStatus(int $value): ?\App\Table\Generated\EventRow
+    {
+        $condition = \PSX\Sql\Condition::withAnd();
+        $condition->equals('status', $value);
+        return $this->doFindOneBy($condition);
+    }
+    /**
+     * @throws \PSX\Sql\Exception\ManipulationException
+     */
+    public function updateByStatus(int $value, \App\Table\Generated\EventRow $record): int
+    {
+        $condition = \PSX\Sql\Condition::withAnd();
+        $condition->equals('status', $value);
+        return $this->doUpdateBy($condition, $record->toRecord());
+    }
+    /**
+     * @throws \PSX\Sql\Exception\ManipulationException
+     */
+    public function deleteByStatus(int $value): int
+    {
+        $condition = \PSX\Sql\Condition::withAnd();
+        $condition->equals('status', $value);
+        return $this->doDeleteBy($condition);
+    }
+    /**
+     * @return array<\App\Table\Generated\EventRow>
+     * @throws \PSX\Sql\Exception\QueryException
+     */
+    public function findByDisplayId(string $value, ?int $startIndex = null, ?int $count = null, ?\App\Table\Generated\EventColumn $sortBy = null, ?\PSX\Sql\OrderBy $sortOrder = null): array
+    {
+        $condition = \PSX\Sql\Condition::withAnd();
+        $condition->like('display_id', $value);
+        return $this->doFindBy($condition, $startIndex, $count, $sortBy, $sortOrder);
+    }
+    /**
+     * @throws \PSX\Sql\Exception\QueryException
+     */
+    public function findOneByDisplayId(string $value): ?\App\Table\Generated\EventRow
+    {
+        $condition = \PSX\Sql\Condition::withAnd();
+        $condition->like('display_id', $value);
+        return $this->doFindOneBy($condition);
+    }
+    /**
+     * @throws \PSX\Sql\Exception\ManipulationException
+     */
+    public function updateByDisplayId(string $value, \App\Table\Generated\EventRow $record): int
+    {
+        $condition = \PSX\Sql\Condition::withAnd();
+        $condition->like('display_id', $value);
+        return $this->doUpdateBy($condition, $record->toRecord());
+    }
+    /**
+     * @throws \PSX\Sql\Exception\ManipulationException
+     */
+    public function deleteByDisplayId(string $value): int
+    {
+        $condition = \PSX\Sql\Condition::withAnd();
+        $condition->like('display_id', $value);
+        return $this->doDeleteBy($condition);
+    }
+    /**
+     * @return array<\App\Table\Generated\EventRow>
+     * @throws \PSX\Sql\Exception\QueryException
+     */
+    public function findByName(string $value, ?int $startIndex = null, ?int $count = null, ?\App\Table\Generated\EventColumn $sortBy = null, ?\PSX\Sql\OrderBy $sortOrder = null): array
+    {
+        $condition = \PSX\Sql\Condition::withAnd();
+        $condition->like('name', $value);
+        return $this->doFindBy($condition, $startIndex, $count, $sortBy, $sortOrder);
+    }
+    /**
+     * @throws \PSX\Sql\Exception\QueryException
+     */
+    public function findOneByName(string $value): ?\App\Table\Generated\EventRow
+    {
+        $condition = \PSX\Sql\Condition::withAnd();
+        $condition->like('name', $value);
+        return $this->doFindOneBy($condition);
+    }
+    /**
+     * @throws \PSX\Sql\Exception\ManipulationException
+     */
+    public function updateByName(string $value, \App\Table\Generated\EventRow $record): int
+    {
+        $condition = \PSX\Sql\Condition::withAnd();
+        $condition->like('name', $value);
+        return $this->doUpdateBy($condition, $record->toRecord());
+    }
+    /**
+     * @throws \PSX\Sql\Exception\ManipulationException
+     */
+    public function deleteByName(string $value): int
+    {
+        $condition = \PSX\Sql\Condition::withAnd();
+        $condition->like('name', $value);
+        return $this->doDeleteBy($condition);
+    }
+    /**
+     * @return array<\App\Table\Generated\EventRow>
+     * @throws \PSX\Sql\Exception\QueryException
+     */
+    public function findByFunderId(int $value, ?int $startIndex = null, ?int $count = null, ?\App\Table\Generated\EventColumn $sortBy = null, ?\PSX\Sql\OrderBy $sortOrder = null): array
+    {
+        $condition = \PSX\Sql\Condition::withAnd();
+        $condition->equals('funder_id', $value);
+        return $this->doFindBy($condition, $startIndex, $count, $sortBy, $sortOrder);
+    }
+    /**
+     * @throws \PSX\Sql\Exception\QueryException
+     */
+    public function findOneByFunderId(int $value): ?\App\Table\Generated\EventRow
+    {
+        $condition = \PSX\Sql\Condition::withAnd();
+        $condition->equals('funder_id', $value);
+        return $this->doFindOneBy($condition);
+    }
+    /**
+     * @throws \PSX\Sql\Exception\ManipulationException
+     */
+    public function updateByFunderId(int $value, \App\Table\Generated\EventRow $record): int
+    {
+        $condition = \PSX\Sql\Condition::withAnd();
+        $condition->equals('funder_id', $value);
+        return $this->doUpdateBy($condition, $record->toRecord());
+    }
+    /**
+     * @throws \PSX\Sql\Exception\ManipulationException
+     */
+    public function deleteByFunderId(int $value): int
+    {
+        $condition = \PSX\Sql\Condition::withAnd();
+        $condition->equals('funder_id', $value);
+        return $this->doDeleteBy($condition);
+    }
+    /**
+     * @return array<\App\Table\Generated\EventRow>
+     * @throws \PSX\Sql\Exception\QueryException
+     */
+    public function findByStartDate(\PSX\DateTime\LocalDateTime $value, ?int $startIndex = null, ?int $count = null, ?\App\Table\Generated\EventColumn $sortBy = null, ?\PSX\Sql\OrderBy $sortOrder = null): array
+    {
+        $condition = \PSX\Sql\Condition::withAnd();
+        $condition->equals('start_date', $value);
+        return $this->doFindBy($condition, $startIndex, $count, $sortBy, $sortOrder);
+    }
+    /**
+     * @throws \PSX\Sql\Exception\QueryException
+     */
+    public function findOneByStartDate(\PSX\DateTime\LocalDateTime $value): ?\App\Table\Generated\EventRow
+    {
+        $condition = \PSX\Sql\Condition::withAnd();
+        $condition->equals('start_date', $value);
+        return $this->doFindOneBy($condition);
+    }
+    /**
+     * @throws \PSX\Sql\Exception\ManipulationException
+     */
+    public function updateByStartDate(\PSX\DateTime\LocalDateTime $value, \App\Table\Generated\EventRow $record): int
+    {
+        $condition = \PSX\Sql\Condition::withAnd();
+        $condition->equals('start_date', $value);
+        return $this->doUpdateBy($condition, $record->toRecord());
+    }
+    /**
+     * @throws \PSX\Sql\Exception\ManipulationException
+     */
+    public function deleteByStartDate(\PSX\DateTime\LocalDateTime $value): int
+    {
+        $condition = \PSX\Sql\Condition::withAnd();
+        $condition->equals('start_date', $value);
+        return $this->doDeleteBy($condition);
+    }
+    /**
+     * @return array<\App\Table\Generated\EventRow>
+     * @throws \PSX\Sql\Exception\QueryException
+     */
+    public function findByEndDate(\PSX\DateTime\LocalDateTime $value, ?int $startIndex = null, ?int $count = null, ?\App\Table\Generated\EventColumn $sortBy = null, ?\PSX\Sql\OrderBy $sortOrder = null): array
+    {
+        $condition = \PSX\Sql\Condition::withAnd();
+        $condition->equals('end_date', $value);
+        return $this->doFindBy($condition, $startIndex, $count, $sortBy, $sortOrder);
+    }
+    /**
+     * @throws \PSX\Sql\Exception\QueryException
+     */
+    public function findOneByEndDate(\PSX\DateTime\LocalDateTime $value): ?\App\Table\Generated\EventRow
+    {
+        $condition = \PSX\Sql\Condition::withAnd();
+        $condition->equals('end_date', $value);
+        return $this->doFindOneBy($condition);
+    }
+    /**
+     * @throws \PSX\Sql\Exception\ManipulationException
+     */
+    public function updateByEndDate(\PSX\DateTime\LocalDateTime $value, \App\Table\Generated\EventRow $record): int
+    {
+        $condition = \PSX\Sql\Condition::withAnd();
+        $condition->equals('end_date', $value);
+        return $this->doUpdateBy($condition, $record->toRecord());
+    }
+    /**
+     * @throws \PSX\Sql\Exception\ManipulationException
+     */
+    public function deleteByEndDate(\PSX\DateTime\LocalDateTime $value): int
+    {
+        $condition = \PSX\Sql\Condition::withAnd();
+        $condition->equals('end_date', $value);
+        return $this->doDeleteBy($condition);
+    }
+    /**
+     * @return array<\App\Table\Generated\EventRow>
+     * @throws \PSX\Sql\Exception\QueryException
+     */
+    public function findByUpdateDate(\PSX\DateTime\LocalDateTime $value, ?int $startIndex = null, ?int $count = null, ?\App\Table\Generated\EventColumn $sortBy = null, ?\PSX\Sql\OrderBy $sortOrder = null): array
+    {
+        $condition = \PSX\Sql\Condition::withAnd();
+        $condition->equals('update_date', $value);
+        return $this->doFindBy($condition, $startIndex, $count, $sortBy, $sortOrder);
+    }
+    /**
+     * @throws \PSX\Sql\Exception\QueryException
+     */
+    public function findOneByUpdateDate(\PSX\DateTime\LocalDateTime $value): ?\App\Table\Generated\EventRow
+    {
+        $condition = \PSX\Sql\Condition::withAnd();
+        $condition->equals('update_date', $value);
+        return $this->doFindOneBy($condition);
+    }
+    /**
+     * @throws \PSX\Sql\Exception\ManipulationException
+     */
+    public function updateByUpdateDate(\PSX\DateTime\LocalDateTime $value, \App\Table\Generated\EventRow $record): int
+    {
+        $condition = \PSX\Sql\Condition::withAnd();
+        $condition->equals('update_date', $value);
+        return $this->doUpdateBy($condition, $record->toRecord());
+    }
+    /**
+     * @throws \PSX\Sql\Exception\ManipulationException
+     */
+    public function deleteByUpdateDate(\PSX\DateTime\LocalDateTime $value): int
+    {
+        $condition = \PSX\Sql\Condition::withAnd();
+        $condition->equals('update_date', $value);
+        return $this->doDeleteBy($condition);
+    }
+    /**
+     * @return array<\App\Table\Generated\EventRow>
+     * @throws \PSX\Sql\Exception\QueryException
+     */
+    public function findByInsertDate(\PSX\DateTime\LocalDateTime $value, ?int $startIndex = null, ?int $count = null, ?\App\Table\Generated\EventColumn $sortBy = null, ?\PSX\Sql\OrderBy $sortOrder = null): array
+    {
+        $condition = \PSX\Sql\Condition::withAnd();
+        $condition->equals('insert_date', $value);
+        return $this->doFindBy($condition, $startIndex, $count, $sortBy, $sortOrder);
+    }
+    /**
+     * @throws \PSX\Sql\Exception\QueryException
+     */
+    public function findOneByInsertDate(\PSX\DateTime\LocalDateTime $value): ?\App\Table\Generated\EventRow
+    {
+        $condition = \PSX\Sql\Condition::withAnd();
+        $condition->equals('insert_date', $value);
+        return $this->doFindOneBy($condition);
+    }
+    /**
+     * @throws \PSX\Sql\Exception\ManipulationException
+     */
+    public function updateByInsertDate(\PSX\DateTime\LocalDateTime $value, \App\Table\Generated\EventRow $record): int
+    {
+        $condition = \PSX\Sql\Condition::withAnd();
+        $condition->equals('insert_date', $value);
+        return $this->doUpdateBy($condition, $record->toRecord());
+    }
+    /**
+     * @throws \PSX\Sql\Exception\ManipulationException
+     */
+    public function deleteByInsertDate(\PSX\DateTime\LocalDateTime $value): int
+    {
+        $condition = \PSX\Sql\Condition::withAnd();
+        $condition->equals('insert_date', $value);
+        return $this->doDeleteBy($condition);
+    }
+    /**
+     * @throws \PSX\Sql\Exception\ManipulationException
+     */
+    public function create(\App\Table\Generated\EventRow $record): int
+    {
+        return $this->doCreate($record->toRecord());
+    }
+    /**
+     * @throws \PSX\Sql\Exception\ManipulationException
+     */
+    public function update(\App\Table\Generated\EventRow $record): int
+    {
+        return $this->doUpdate($record->toRecord());
+    }
+    /**
+     * @throws \PSX\Sql\Exception\ManipulationException
+     */
+    public function updateBy(\PSX\Sql\Condition $condition, \App\Table\Generated\EventRow $record): int
+    {
+        return $this->doUpdateBy($condition, $record->toRecord());
+    }
+    /**
+     * @throws \PSX\Sql\Exception\ManipulationException
+     */
+    public function delete(\App\Table\Generated\EventRow $record): int
+    {
+        return $this->doDelete($record->toRecord());
+    }
+    /**
+     * @throws \PSX\Sql\Exception\ManipulationException
+     */
+    public function deleteBy(\PSX\Sql\Condition $condition): int
+    {
+        return $this->doDeleteBy($condition);
+    }
+    /**
+     * @param array<string, mixed> $row
+     */
+    protected function newRecord(array $row): \App\Table\Generated\EventRow
+    {
+        return \App\Table\Generated\EventRow::from($row);
+    }
 }
